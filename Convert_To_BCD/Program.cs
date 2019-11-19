936a079325904e299afb458cd119017f6fb807f6
PopoviciSerban <serbangaming24@gmail.com>
2019-11-19 02:30:28 +0200

    Add project files.

---
+++ Convert_To_BCD/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convert_To_BCD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, c;
+            string[] a = { "0000", "0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000", "1001" };
+
+            Stack<int> myStack = new Stack<int>();
+            StringBuilder result = new StringBuilder();
+
+            number = int.Parse(Console.ReadLine());
+
+            while (number != 0)
+            {
+                c = number % 10;
+                myStack.Push(c);
+                number /= 10;
+            }
+
+            while (myStack.Count != 0)
+                result.Append(a[myStack.Pop()] + " ");
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
